7d58b50413085c47a44bbf9b8592ff31b380eec9
kihun.ahn <kihun.ahn@samsung.com>
2024-08-01 21:18:31 +0900

    Add SetTitle()

---
+++ Pt5Viewer/MainForm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pt5Viewer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            SetTitle();
+        }
+
+        private void SetTitle()
+        {
+            Version version = Assembly.GetEntryAssembly().GetName().Version;
+            
+            string title = $"[{Id}] {Application.ProductName} v{version.Major}.{version.Minor}.{version.Build}";
+            if (version.Revision != 0)
+            {
+                title = $"{title}.{version.Revision}";
+            }
+
+            Text = title;
+        }
+
+        public static int Id
+        {
+            get
+            {
+                string processName = Process.GetCurrentProcess().ProcessName;
+
+                Process[] processes = Process.GetProcessesByName(processName);
+                if (processes.Length > 1)
+                {
+                    return Process.GetCurrentProcess().Id;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+    }
+}
--- Pt5Viewer/Program.cs
+++ Pt5Viewer/Program.cs
@@ -16,7 +16,7 @@ namespace Pt5Viewer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
