37a8de2e1e7941c2fa306ebd535484de93b5bc3f
kihun.ahn <khahn0721@gmail.com>
2024-09-29 18:31:44 +0900

    Add PowerToolScaleView

---
+++ Pt5Viewer/Views/PowerToolScaleView.Designer.cs
@@ -0,0 +1,280 @@
+﻿
+namespace Pt5Viewer.Views
+{
+    partial class PowerToolScaleView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBoxTimeUnit = new System.Windows.Forms.ComboBox();
+            this.comboBoxTimeUnitsPerTick = new System.Windows.Forms.ComboBox();
+            this.comboBoxTimeNumberOfTicks = new System.Windows.Forms.ComboBox();
+            this.comboBoxCurrentUnit = new System.Windows.Forms.ComboBox();
+            this.comboBoxCurrentUnitsPerTick = new System.Windows.Forms.ComboBox();
+            this.comboBoxCurrentNumberOfTicks = new System.Windows.Forms.ComboBox();
+            this.textBoxTimeOffset = new System.Windows.Forms.TextBox();
+            this.textBoxCurrentOffset = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.ColumnCount = 11;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 5F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 70F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 10F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 10F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 10F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 10F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 21F));
+            this.tableLayoutPanel.Controls.Add(this.label1, 1, 3);
+            this.tableLayoutPanel.Controls.Add(this.label2, 1, 5);
+            this.tableLayoutPanel.Controls.Add(this.label3, 3, 1);
+            this.tableLayoutPanel.Controls.Add(this.label4, 5, 1);
+            this.tableLayoutPanel.Controls.Add(this.label5, 7, 1);
+            this.tableLayoutPanel.Controls.Add(this.label6, 9, 1);
+            this.tableLayoutPanel.Controls.Add(this.comboBoxTimeUnit, 3, 3);
+            this.tableLayoutPanel.Controls.Add(this.comboBoxTimeUnitsPerTick, 5, 3);
+            this.tableLayoutPanel.Controls.Add(this.comboBoxTimeNumberOfTicks, 7, 3);
+            this.tableLayoutPanel.Controls.Add(this.comboBoxCurrentUnit, 3, 5);
+            this.tableLayoutPanel.Controls.Add(this.comboBoxCurrentUnitsPerTick, 5, 5);
+            this.tableLayoutPanel.Controls.Add(this.comboBoxCurrentNumberOfTicks, 7, 5);
+            this.tableLayoutPanel.Controls.Add(this.textBoxTimeOffset, 9, 3);
+            this.tableLayoutPanel.Controls.Add(this.textBoxCurrentOffset, 9, 5);
+            this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.RowCount = 7;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 5F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 10F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 10F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(484, 161);
+            this.tableLayoutPanel.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(8, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Time";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(8, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Current";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Location = new System.Drawing.Point(88, 5);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Unit";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Location = new System.Drawing.Point(185, 5);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Units/tick";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label5.Location = new System.Drawing.Point(282, 5);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "#Ticks";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label6.Location = new System.Drawing.Point(379, 5);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Offset";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // comboBoxTimeUnit
+            // 
+            this.comboBoxTimeUnit.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBoxTimeUnit.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxTimeUnit.FormattingEnabled = true;
+            this.comboBoxTimeUnit.Location = new System.Drawing.Point(85, 35);
+            this.comboBoxTimeUnit.Margin = new System.Windows.Forms.Padding(0);
+            this.comboBoxTimeUnit.Name = "comboBoxTimeUnit";
+            this.comboBoxTimeUnit.Size = new System.Drawing.Size(87, 20);
+            this.comboBoxTimeUnit.TabIndex = 6;
+            this.comboBoxTimeUnit.SelectedIndexChanged += new System.EventHandler(this.comboBoxTimeScale_SelectedIndexChanged);
+            // 
+            // comboBoxTimeUnitsPerTick
+            // 
+            this.comboBoxTimeUnitsPerTick.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBoxTimeUnitsPerTick.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxTimeUnitsPerTick.FormattingEnabled = true;
+            this.comboBoxTimeUnitsPerTick.Location = new System.Drawing.Point(182, 35);
+            this.comboBoxTimeUnitsPerTick.Margin = new System.Windows.Forms.Padding(0);
+            this.comboBoxTimeUnitsPerTick.Name = "comboBoxTimeUnitsPerTick";
+            this.comboBoxTimeUnitsPerTick.Size = new System.Drawing.Size(87, 20);
+            this.comboBoxTimeUnitsPerTick.TabIndex = 7;
+            // 
+            // comboBoxTimeNumberOfTicks
+            // 
+            this.comboBoxTimeNumberOfTicks.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBoxTimeNumberOfTicks.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxTimeNumberOfTicks.FormattingEnabled = true;
+            this.comboBoxTimeNumberOfTicks.Location = new System.Drawing.Point(279, 35);
+            this.comboBoxTimeNumberOfTicks.Margin = new System.Windows.Forms.Padding(0);
+            this.comboBoxTimeNumberOfTicks.Name = "comboBoxTimeNumberOfTicks";
+            this.comboBoxTimeNumberOfTicks.Size = new System.Drawing.Size(87, 20);
+            this.comboBoxTimeNumberOfTicks.TabIndex = 8;
+            // 
+            // comboBoxCurrentUnit
+            // 
+            this.comboBoxCurrentUnit.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBoxCurrentUnit.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxCurrentUnit.FormattingEnabled = true;
+            this.comboBoxCurrentUnit.Location = new System.Drawing.Point(85, 65);
+            this.comboBoxCurrentUnit.Margin = new System.Windows.Forms.Padding(0);
+            this.comboBoxCurrentUnit.Name = "comboBoxCurrentUnit";
+            this.comboBoxCurrentUnit.Size = new System.Drawing.Size(87, 20);
+            this.comboBoxCurrentUnit.TabIndex = 8;
+            this.comboBoxCurrentUnit.SelectedIndexChanged += new System.EventHandler(this.comboBoxCurrentScale_SelectedIndexChanged);
+            // 
+            // comboBoxCurrentUnitsPerTick
+            // 
+            this.comboBoxCurrentUnitsPerTick.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBoxCurrentUnitsPerTick.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxCurrentUnitsPerTick.FormattingEnabled = true;
+            this.comboBoxCurrentUnitsPerTick.Location = new System.Drawing.Point(182, 65);
+            this.comboBoxCurrentUnitsPerTick.Margin = new System.Windows.Forms.Padding(0);
+            this.comboBoxCurrentUnitsPerTick.Name = "comboBoxCurrentUnitsPerTick";
+            this.comboBoxCurrentUnitsPerTick.Size = new System.Drawing.Size(87, 20);
+            this.comboBoxCurrentUnitsPerTick.TabIndex = 8;
+            // 
+            // comboBoxCurrentNumberOfTicks
+            // 
+            this.comboBoxCurrentNumberOfTicks.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBoxCurrentNumberOfTicks.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxCurrentNumberOfTicks.FormattingEnabled = true;
+            this.comboBoxCurrentNumberOfTicks.Location = new System.Drawing.Point(279, 65);
+            this.comboBoxCurrentNumberOfTicks.Margin = new System.Windows.Forms.Padding(0);
+            this.comboBoxCurrentNumberOfTicks.Name = "comboBoxCurrentNumberOfTicks";
+            this.comboBoxCurrentNumberOfTicks.Size = new System.Drawing.Size(87, 20);
+            this.comboBoxCurrentNumberOfTicks.TabIndex = 8;
+            // 
+            // textBoxTimeOffset
+            // 
+            this.textBoxTimeOffset.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxTimeOffset.Location = new System.Drawing.Point(376, 35);
+            this.textBoxTimeOffset.Margin = new System.Windows.Forms.Padding(0);
+            this.textBoxTimeOffset.Name = "textBoxTimeOffset";
+            this.textBoxTimeOffset.Size = new System.Drawing.Size(87, 21);
+            this.textBoxTimeOffset.TabIndex = 9;
+            this.textBoxTimeOffset.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxTimeOffset_KeyDown);
+            // 
+            // textBoxCurrentOffset
+            // 
+            this.textBoxCurrentOffset.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxCurrentOffset.Location = new System.Drawing.Point(376, 65);
+            this.textBoxCurrentOffset.Margin = new System.Windows.Forms.Padding(0);
+            this.textBoxCurrentOffset.Name = "textBoxCurrentOffset";
+            this.textBoxCurrentOffset.Size = new System.Drawing.Size(87, 21);
+            this.textBoxCurrentOffset.TabIndex = 10;
+            this.textBoxCurrentOffset.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxCurrentOffset_KeyDown);
+            // 
+            // PowerToolScaleView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 161);
+            this.Controls.Add(this.tableLayoutPanel);
+            this.Name = "PowerToolScaleView";
+            this.Text = "ScaleView";
+            this.tableLayoutPanel.ResumeLayout(false);
+            this.tableLayoutPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBoxTimeUnit;
+        private System.Windows.Forms.ComboBox comboBoxTimeUnitsPerTick;
+        private System.Windows.Forms.ComboBox comboBoxTimeNumberOfTicks;
+        private System.Windows.Forms.ComboBox comboBoxCurrentUnit;
+        private System.Windows.Forms.ComboBox comboBoxCurrentUnitsPerTick;
+        private System.Windows.Forms.ComboBox comboBoxCurrentNumberOfTicks;
+        private System.Windows.Forms.TextBox textBoxTimeOffset;
+        private System.Windows.Forms.TextBox textBoxCurrentOffset;
+    }
+}
---
+++ Pt5Viewer/Views/PowerToolScaleView.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Pt5Viewer.Common;
+using Pt5Viewer.Enums;
+
+namespace Pt5Viewer.Views
+{
+    public partial class PowerToolScaleView : DockForm
+    {
+        public PowerToolScaleView()
+        {
+            InitializeComponent();
+
+            comboBoxTimeUnit.DataSource = Enum.GetValues(typeof(TimeUnitEnum))
+                                                .Cast<TimeUnitEnum>()
+                                                .Select(e => new { Value = e, Description = Util.GetEnumDescription(e) })
+                                                .ToList();
+            comboBoxTimeUnit.DisplayMember = "Description";
+            comboBoxTimeUnit.ValueMember = "Value";
+
+            comboBoxTimeUnitsPerTick.DataSource = Enum.GetValues(typeof(TimeUnitsPerTickEnum))
+                                                        .Cast<TimeUnitsPerTickEnum>()
+                                                        .Select(e => new { Value = e, Description = Util.GetEnumDescription(e) })
+                                                        .ToList();
+            comboBoxTimeUnitsPerTick.DisplayMember = "Description";
+            comboBoxTimeUnitsPerTick.ValueMember = "Value";
+
+            comboBoxTimeNumberOfTicks.DataSource = Enum.GetValues(typeof(TimeNumberOfTicksEnum))
+                                                        .Cast<TimeNumberOfTicksEnum>()
+                                                        .Select(e => new { Value = e, Description = Util.GetEnumDescription(e) })
+                                                        .ToList();
+            comboBoxTimeNumberOfTicks.DisplayMember = "Description";
+            comboBoxTimeNumberOfTicks.ValueMember = "Value";
+
+            comboBoxCurrentUnit.Items.AddRange(Constant.CurrentUnitList.ToArray());
+            comboBoxCurrentUnitsPerTick.Items.AddRange(Constant.CurrentUnitsPerTickList.ToArray());
+            comboBoxCurrentNumberOfTicks.Items.AddRange(Constant.CurrentNumberOfTicksList.ToArray());
+        }
+
+        public TimeUnitEnum TimeUnit
+        {
+            get => (TimeUnitEnum)comboBoxTimeUnit.SelectedValue;
+            set => comboBoxTimeUnit.SelectedValue = value;
+        }
+
+        public TimeUnitsPerTickEnum TimeUnitsPerTick
+        {
+            get => (TimeUnitsPerTickEnum)comboBoxTimeUnitsPerTick.SelectedValue;
+            set => comboBoxTimeUnitsPerTick.SelectedValue = value;
+        }
+
+        public TimeNumberOfTicksEnum TimeNumberOfTicks
+        {
+            get => (TimeNumberOfTicksEnum)comboBoxTimeNumberOfTicks.SelectedValue;
+            set => comboBoxTimeNumberOfTicks.SelectedValue = value;
+        }
+
+        public string TimeOffset
+        {
+            get => textBoxTimeOffset.Text;
+            set => textBoxTimeOffset.Text = value;
+        }
+
+        public string CurrentUnit
+        {
+            get => comboBoxCurrentUnit.SelectedItem?.ToString();
+            set => comboBoxCurrentUnit.SelectedItem = value;
+        }
+
+        public string CurrentUnitsPerTick
+        {
+            get => comboBoxCurrentUnitsPerTick.SelectedItem?.ToString();
+            set => comboBoxCurrentUnitsPerTick.SelectedItem = value;
+        }
+
+        public string CurrentNumberOfTicks
+        {
+            get => comboBoxCurrentNumberOfTicks.SelectedItem?.ToString();
+            set => comboBoxCurrentNumberOfTicks.SelectedItem = value;
+        }
+
+        public string CurrentOffset
+        {
+            get => textBoxCurrentOffset.Text;
+            set => textBoxCurrentOffset.Text = value;
+        }
+
+        public event EventHandler TimeScaleChanged;
+        public event EventHandler TimeOffsetChanged;
+
+        public event EventHandler CurrentScaleChanged;
+        public event EventHandler CurrentOffsetChanged;
+
+        private void comboBoxTimeScale_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            TimeScaleChanged?.Invoke(sender, e);
+        }
+
+        private void comboBoxCurrentScale_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CurrentScaleChanged?.Invoke(sender, e);
+        }
+
+        private void textBoxTimeOffset_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                TimeOffsetChanged?.Invoke(sender, e);
+            }
+        }
+
+        private void textBoxCurrentOffset_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                CurrentOffsetChanged?.Invoke(sender, e);
+            }
+        }
+    }
+}
